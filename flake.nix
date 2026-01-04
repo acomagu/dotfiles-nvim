8550a667b4087b5d35c55313050e0eeb70872b4f
Yuki Ito <mail@acomagu.me>
2026-01-04 23:33:40 +0900

    add color

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     # home-manager から import できるモジュールを公開
     homeManagerModules.default = { ... }: {
       home.file.".config/nvim/init.lua".source = self + "/init.lua";
+      home.file.".config/nvim/colors/minicomment.vim".source = self + "/colors/minicomment.vim";
     };
   };
 }
